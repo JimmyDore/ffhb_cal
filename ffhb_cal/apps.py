0b6768e5f0f7b9ce50390cc5a7098308c13d6b48
jimmydore <jimmy291295@gmail.com>
2018-10-18 22:23:10 +0100

    Initialize Django Project

---
+++ ffhb_cal/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FfhbCalConfig(AppConfig):
+    name = 'ffhb_cal'
---
+++ ffhb_cal_app/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ffhb_cal_app project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ffhb_cal_app.settings')
+
+application = get_wsgi_application()
