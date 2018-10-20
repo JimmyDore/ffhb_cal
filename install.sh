3211d4e8572a84814e5b5cfca1c3290ee1137b4a
Jimmy Doré <jimmy291295@gmail.com>
2018-10-20 06:01:15 +0200

    added install script to get last version of repo and install it : works for server and dev, and for sync with multiple branches

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git pull origin master
+cd ffhb_cal || exit
+cp ffhb_cal/conf.local.py ffhb_cal/conf.py
+source ../ffhb_venv/bin/activate
+pip install -r ../requirements.txt
+python manage.py makemigrations ffhb_cal_app
+python manage.py migrate
+if [ "$1" == "prod" ]
+then
+        sudo systemctl restart gunicorn_ffhb_cal.service
+else
+        python manage.py runserver
+fi
