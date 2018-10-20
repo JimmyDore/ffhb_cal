f29b6dd02e920182a0cd0b4e2ad998a7f3cf2864
jimmydore <jimmy291295@gmail.com>
2018-10-20 04:16:10 +0100

    fixed access to static files from dev pov

--- ffhb_cal/ffhb_cal/settings.py
+++ ffhb_cal/ffhb_cal/settings.py
@@ -136,8 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "build", "static"),
+    os.path.join(BASE_DIR, "ffhb_cal", "static"),
+    os.path.join(BASE_DIR, "ffhb_cal_app", "static"),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
